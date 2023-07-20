86110f7f47cbadc18f92753e289ffd23e4f3130c
Gbzzz <guilhermebraz089@gmail.com>
2023-07-20 19:37:27 -0300

    update docker-compose

--- ficker-back-end/resources/views/layouts/app.blade.php
+++ ficker-back-end/resources/views/layouts/app.blade.php
@@ -13,7 +13,6 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @vite('resources/js/app.js')
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
