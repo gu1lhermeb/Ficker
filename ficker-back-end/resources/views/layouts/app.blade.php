ad9527c032c3ae49e44e4246253db2bc2cf5d32a
Gbzzz <guilhermebraz089@gmail.com>
2023-07-25 10:21:57 -0300

    git revert HEAD^

--- ficker-back-end/resources/views/layouts/app.blade.php
+++ ficker-back-end/resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite('resources/js/app.js')
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
