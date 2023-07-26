cb8361ab841cb20c5218d482d08b7fc931cc6c1f
alandysson <alandyssom@gmail.com>
2023-07-26 13:48:19 -0300

    feat: adiciona novo mais conteúdo para a tela inicial e cria tela de cadastro

---
+++ ficker-front/src/app/createaccount/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+const CreateAccountPage = () => {
+  return (
+    <div>
+      <div style={{ background: "#fff", padding: 10, alignItems: "center" }}>
+        <Image src="/logo.png" alt="Logo" width={130} height={30} />
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default CreateAccountPage;
