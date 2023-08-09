5b168fd2221a1686cb7e1eb90418dfecc262dfb6
PHPdro <pedro110302@gmail.com>
2023-08-09 19:13:26 -0300

    add: TransactionTest

---
+++ ficker-back-end/app/Http/Controllers/TransactionController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Transaction;
+use App\Models\Category;
+
+class TransactionController extends Controller
+{
+
+    public function store(Request $request) : JsonResponse 
+    {
+
+        $request->validate([
+            'description' => ['required', 'string', 'max:255'],
+            'date' => ['required', 'date'],
+            'category_id' => ['required'],
+            'value' => ['required', 'decimal:0,2']
+        ]);
+        
+        if($request->category_id == '0') { // Assumindo que o value da option NOVA seja 0
+
+            $request->validate([
+                'category_description' => ['required', 'string', 'max:255'],
+            ]);
+
+            $category = Category::create([
+                'category_description' => $request->category_description,
+                'type' => $request->type
+            ]);
+
+        } else {
+
+            $category = Category::find($request->category_id); // Assumindo que o value das options sejam o respectivo id da categoria
+        }
+
+        $transaction = Transaction::create([
+            'user_id' => Auth::user()->id,
+            'category_id' => $category->id,
+            'description' => $request->description,
+            'date' => $request->date,
+            'type' => $request->type,
+            'value' => $request->value
+        ]);
+
+        $response = [
+            'transaction' => $transaction
+        ];
+
+        return response()->json($response, 201);
+    }
+}
