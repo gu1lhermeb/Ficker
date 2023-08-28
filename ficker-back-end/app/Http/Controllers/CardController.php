ef9e9dcebbe626eabefeedea8e4e705b13845e72
Gbzzz <guilhermebraz089@gmail.com>
2023-08-28 12:11:35 -0300

    add: try e cath no CardController

--- ficker-back-end/app/Http/Controllers/CardController.php
+++ ficker-back-end/app/Http/Controllers/CardController.php
@@ -15,59 +15,88 @@ class CardController extends Controller
 {
     public function store(Request $request) : JsonResponse
     {
+        try {
+            $request->validate([
+                'description' => ['required', 'string', 'min:2', 'max:50'],
+                'flag_id' => ['required'],
+                'expiration' => ['required', 'integer', 'min:1', 'max:31'],
+            ]);
 
-        $request->validate([
-            'description' => ['required', 'string', 'min:2', 'max:50'],
-            'flag_id' => ['required'],
-            'expiration' => ['required', 'integer', 'min:1', 'max:31'],
-        ]);
+            $expiration = $request->expiration;
+            $best_day = $expiration - 9;
 
-        $expiration = $request->expiration;
-        $best_day = $expiration - 9;
+            if($best_day < 0){
+                $last_month = strtotime('last month');
+                $days_last_month = date("t", $last_month);
+                $days_last_month_int = (int) $days_last_month;
+                $best_day = $days_last_month_int - abs($best_day);
+            } elseif($expiration == 9){
+                $last_month_last_day = date("d", strtotime("last day of last month"));
+                $best_day = (int)$last_month_last_day;
+            }
 
-        if($best_day < 0){
-            $last_month = strtotime('last month');
-            $days_last_month = date("t", $last_month);
-            $days_last_month_int = (int) $days_last_month;
-            $best_day = $days_last_month_int - abs($best_day);
-        } elseif($expiration == 9){
-            $last_month_last_day = date("d", strtotime("last day of last month"));
-            $best_day = (int)$last_month_last_day;
+            $card = Card::create([
+                'user_id' => Auth::user()->id,
+                'flag_id' => $request->flag_id,
+                'description' => $request->description,
+                'expiration' => $request->expiration,
+                'best_day' => $best_day
+            ]);
+
+            $response = [
+                'card' => $card
+            ];
+
+            return response()->json($response, 201);
+        } catch (\Exception $e) {
+            $errorMessage = "Erro ao Criar Cartão";
+            $response = [
+                "error" => $errorMessage
+            ];
+            return response()->json($errorMessage);
         }
-
-        $card = Card::create([
-            'user_id' => Auth::user()->id,
-            'flag_id' => $request->flag_id,
-            'description' => $request->description,
-            'expiration' => $request->expiration,
-            'best_day' => $best_day
-        ]);
-
-        $response = [
-            'card' => $card
-        ];
-
-        return response()->json($response, 201);
     }
 
     public function showCards() :JsonResponse
     {
-        $cards = Auth::user()->cards;
-        $response = [];
-        foreach($cards as $card){
-            array_push($response, $card);
+        try {
+
+            $cards = Auth::user()->cards;
+            $response = [];
+            foreach($cards as $card){
+                array_push($response, $card);
+            }
+            return response()->json($response, 200);
+
+        } catch (\Exception $e) {
+            $errorMessage = "Nenhum cartão cadastrado";
+            $response = [
+                "error" => $errorMessage
+            ];
+            return response()->json($response, 404);
         }
-        return response()->json($response, 200);
+
     }
 
     public function showFlags() :JsonResponse
     {
-        $flags = Flag::all();
-        $response = [];
-        foreach($flags as $flag){
-            array_push($response, $flag);
+        try {
+
+            $flags = Flag::all();
+            $response = [];
+            foreach($flags as $flag){
+                array_push($response, $flag);
+            }
+            return response()->json($response, 200);
+
+        } catch (\Exception $e) {
+            $errorMessage = "Nenhuma bandeira foi encontrada";
+            $response = [
+                "error" => $errorMessage
+            ];
+
+            return response()->json($response, 404);
         }
-        return response()->json($response, 200);
     }
 
     public function showBestDay(Request $request) :JsonResponse
