a0fe5193a268935f9bb0898be7fc59fc2058a55a
Gbzzz <guilhermebraz089@gmail.com>
2023-08-30 12:04:56 -0300

    add: usuário, cartão e transações no seeder

--- ficker-back-end/database/seeders/DatabaseSeeder.php
+++ ficker-back-end/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,11 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use App\Models\Card;
+use App\Models\Transaction;
+use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -48,5 +53,47 @@ class DatabaseSeeder extends Seeder
             \App\Models\Flag::create($flag);
 
         });
+
+        $admin = User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin123'),
+        ]);
+
+        $card = Card::create([
+            'user_id' => $admin->id,
+            'flag_id' => 4,
+            'description' => 'Cartão Nubank',
+            'expiration' => 1,
+        ]);
+
+        Category::create([
+            'category_description' => 'Entrada',
+        ]);
+
+        Category::create([
+            'category_description' => 'Saida',
+        ]);
+
+        Transaction::create([
+            'user_id' => $admin->id,
+            'description' => 'Salário',
+            'date' => '2023-01-03',
+            'type' => 'Entrada',
+            'value' => 1500,
+            'category_id' => 1,
+            'description' => 'Entrada'
+        ]);
+
+        Transaction::create([
+            'user_id' => $admin->id,
+            'description' => 'Compra na Adidas',
+            'date' => '2023-01-03',
+            'type' => 'Saida',
+            'value' => 300,
+            'category_id' => 2,
+            'description' => 'Saida',
+            'card_id' => $card->id,
+        ]);
     }
 }
