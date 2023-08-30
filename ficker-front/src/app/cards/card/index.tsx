6ee98ca1633a64889e4ffad26b225ece0d921724
alandysson <alandyssom@gmail.com>
2023-08-30 15:49:42 -0300

    style: ajusta o tamanho dos componentes a depender da tela

--- ficker-front/src/app/cards/card/index.tsx
+++ ficker-front/src/app/cards/card/index.tsx
@@ -49,7 +49,7 @@ function CardPage({ card }: CardProps) {
     <Col xl={24}>
       <CardTransactionModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
       <Row>
-        <Col xl={14}>
+        <Col xl={14} lg={20} md={20} xs={20}>
           <TransactionTab
             data={[
               {
@@ -68,11 +68,11 @@ function CardPage({ card }: CardProps) {
             ]}
           />
         </Col>
-        <Col xl={6}>
+        <Col xl={6} lg={12} md={20} xs={21}>
           <Col>
             <CardInformation card={card} totalValue={totalValue} />
           </Col>
-          <Col xl={22}>
+          <Col xl={22} lg={22} md={20} xs={21}>
             <Row justify={"end"}>
               <button className={styles.button} onClick={openModal}>
                 Nova transação
