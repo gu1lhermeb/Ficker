802e4bef9bb2135c0480ee6426d9c4ab318b81ae
PHPdro <pedro110302@gmail.com>
2023-09-01 00:26:25 -0300

    add: installments table

---
+++ ficker-back-end/app/Models/Installment.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Installment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'transaction_id',
+        'description',
+        'value',
+        'pay_day'
+    ];
+
+    public function transaction(): BelongsTo
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+}
---
+++ ficker-back-end/database/migrations/2023_08_31_231409_create_installments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('installments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaction_id')->constrained('transactions')->onDelete('cascade');
+            $table->string('description');
+            $table->double('value');
+            $table->date('pay_day');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('installments');
+    }
+};
