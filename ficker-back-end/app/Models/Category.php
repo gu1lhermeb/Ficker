674be670d6d44c29ce68802eac360afd7508d7ed
PHPdro <pedro110302@gmail.com>
2023-09-02 20:20:50 -0300

    update: categories and type relationship

--- ficker-back-end/app/Models/Category.php
+++ ficker-back-end/app/Models/Category.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Category extends Model
 {
@@ -20,4 +21,8 @@ class Category extends Model
         return $this->hasMany(Transaction::class);
     }
     
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
--- ficker-back-end/app/Models/Type.php
+++ ficker-back-end/app/Models/Type.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Type extends Model
 {
@@ -12,4 +13,11 @@ class Type extends Model
     protected $fillable = [
         'description'
     ];
+
+    public function categories(): HasMany
+    {
+        return $this->hasMany(Category::class);
+
+    }
+
 }
