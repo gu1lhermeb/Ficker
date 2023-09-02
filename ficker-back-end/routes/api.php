d6861b16b784fae4c604555a40bc8789378e1903
PHPdro <pedro110302@gmail.com>
2023-09-02 20:20:12 -0300

    add: new transactions and categories routes

--- ficker-back-end/routes/api.php
+++ ficker-back-end/routes/api.php
@@ -23,10 +23,13 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 
 Route::middleware(['auth:sanctum'])->group(function(){
     Route::post('/transaction', [TransactionController::class, 'store']);
-    Route::get('/transactions', [TransactionController::class, 'showTransactions']);
-    Route::get('/categories', [TransactionController::class, 'showCategories']);
+    Route::get('/transactions/incomes', [TransactionController::class, 'showIncomes']); // Entradas
+    Route::get('/transactions/outgoings', [TransactionController::class, 'showOutgoings']); // Saídas
+    Route::get('/transactions/{id}', [TransactionController::class, 'showTransactionInstallments']); // Parcelas de uma transação
+    Route::get('/categories/{id}', [TransactionController::class, 'showCategories']); // Categorias de entrada (1), saída (2) ou cartão de crédito (3)
     Route::post('/card', [CardController::class, 'store']);
     Route::get('/cards', [CardController::class, 'showCards']);
+    Route::get('/cards/{id}', [TransactionController::class, 'showCardTransactions']); // Transações de um cartão de crédito
     Route::get('/flags', [CardController::class, 'showFlags']);
     Route::post('/best/day', [CardController::class, 'showBestDay']);
     Route::post('/invoice/card', [CardController::class, 'invoiceCard']);
