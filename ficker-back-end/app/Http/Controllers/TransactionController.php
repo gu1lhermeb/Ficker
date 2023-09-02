0ee13aee12b86a6d962df39e3862d9b090bfd8f4
PHPdro <pedro110302@gmail.com>
2023-09-02 20:21:54 -0300

    add: new return methods

--- ficker-back-end/app/Http/Controllers/TransactionController.php
+++ ficker-back-end/app/Http/Controllers/TransactionController.php
@@ -9,6 +9,7 @@ use App\Models\Transaction;
 use App\Models\Category;
 use App\Models\Card;
 use App\Models\Installment;
+use App\Models\Type;
 use Carbon\Carbon;
 
 class TransactionController extends Controller
@@ -50,7 +51,7 @@ class TransactionController extends Controller
         if ($request->category_id == '0') {
 
             $request->validate([
-                'category_description' => ['required', 'string', 'max:255'],
+                'category_description' => ['required', 'string', 'max:50', 'unique:App\Models\Category,category_description'],
             ]);
 
             $category = Category::create([
@@ -83,80 +84,181 @@ class TransactionController extends Controller
 
             return response()->json($response, 201);
 
-        } else {
+        } else { // Com parcelas
 
-            if (!(is_null($request->installments))) { // Com parcelas
+            $transaction = Transaction::create([
+                'user_id' => Auth::user()->id,
+                'category_id' => $category->id,
+                'card_id' => $request->card_id,
+                'description' => $request->description,
+                'date' => $request->date,
+                'value' => $request->value,
+                'installments' => $request->installments,
+                'type_id' => $request->type_id
+            ]);
 
-                $transaction = Transaction::create([
-                    'user_id' => Auth::user()->id,
-                    'category_id' => $category->id,
-                    'card_id' => $request->card_id,
-                    'description' => $request->description,
-                    'date' => $request->date,
-                    'value' => $request->value,
-                    'installments' => $request->installments,
-                    'type_id' => $request->type_id
-                ]);
+            $response = [];
+            $pay_day = date('Y-m-d');
+            $new_pay_day_formated = $pay_day;
+            $i = $request->installments;
+            $value = (float)$request->value / (float)$request->installments;
 
-                $response = [];
-                $pay_day = date('Y-m-d');
-                $new_pay_day_formated = $pay_day;
-                $i = $request->installments;
-                $value = (float)$request->value / (float)$request->installments;
+            for($i = 1; $i <= $request->installments; $i++){
 
-                for($i = 1; $i <= $request->installments; $i++){
+                if($i == 1){
+                    $installment = Installment::create([
+                        'transaction_id' => $transaction->id,
+                        'description' => $request->description,
+                        'value' => $value,
+                        'card_id' => $request->card_id,
+                        'pay_day' => $pay_day
+                    ]);
 
-                    if($i == 1){
-                        $installment = Installment::create([
-                            'transaction_id' => $transaction->id,
-                            'description' => $request->description,
-                            'value' => $value,
-                            'card_id' => $request->card_id,
-                            'pay_day' => $pay_day
-                        ]);
+                    array_push($response, $installment);
 
-                        array_push($response, $installment);
+                } else {
+                    $new_pay_day = strtotime('+1 months', strtotime($pay_day));
+                    $new_pay_day_formated = date('Y-m-d', $new_pay_day);
+                    $installment = Installment::create([
+                        'transaction_id' => $transaction->id,
+                        'description' => $request->description,
+                        'value' => $value,
+                        'card_id' => $request->card_id,
+                        'pay_day' => $new_pay_day_formated
+                    ]);
 
-                    } else {
-                        $new_pay_day = strtotime('+1 months', strtotime($pay_day));
-                        $new_pay_day_formated = date('Y-m-d', $new_pay_day);
-                        $installment = Installment::create([
-                            'transaction_id' => $transaction->id,
-                            'description' => $request->description,
-                            'value' => $value,
-                            'card_id' => $request->card_id,
-                            'pay_day' => $new_pay_day_formated
-                        ]);
-
-                        array_push($response, $installment);
-                    }
-
-                    $pay_day = $new_pay_day_formated;
-
-                    }
-
-                    return response()->json($response, 200);
+                    array_push($response, $installment);
                 }
+
+                $pay_day = $new_pay_day_formated;
+
+                }
+
+                return response()->json($response, 200);
             }
         }
 
-    public function showCategories(): JsonResponse
+    public function showCategories($id): JsonResponse
     {
-        $categories = Category::all();
+        $categories = Type::find($id)->categories;
+
         $response = [];
         foreach($categories as $category){
             array_push($response, $category);
         }
+        
         return response()->json($response, 200);
     }
 
-    public function showTransactions(): JsonResponse
+    public function showIncomes(): JsonResponse
     {
-        $transactions = Auth::user()->transactions;
-        $response = [];
-        foreach($transactions  as $transaction){
-            array_push($response, $transaction);
+        try {
+
+            $transactions = Transaction::where([
+                'user_id' => Auth::user()->id,
+                'type_id' => 1
+            ]);
+    
+            $response = [];
+            foreach($transactions  as $transaction){
+                array_push($response, $transaction);
+            }
+    
+            return response()->json($response, 200);
+
+        } catch(\Exception $e) {
+
+            $errorMessage = "Nenhuma transação de entrada encontrada.";
+            $response = [
+                "data" => [
+                    "error" => $errorMessage
+                ]
+            ];
+            return response()->json($response, 404);
+
         }
-        return response()->json($response, 200);
+    }
+
+    public function showOutgoings(): JsonResponse
+    {
+        try {
+
+            $transactions = Transaction::where([
+                'user_id' => Auth::user()->id,
+                'type_id' => 2
+            ]);
+    
+            $response = [];
+            foreach($transactions  as $transaction){
+                array_push($response, $transaction);
+            }
+    
+            return response()->json($response, 200);
+
+        } catch(\Exception $e) {
+
+            $errorMessage = "Nenhuma transação de saída encontrada.";
+            $response = [
+                "data" => [
+                    "error" => $errorMessage
+                ]
+            ];
+            return response()->json($response, 404);
+        }
+    }
+
+    public function showCardTransactions($id): JsonResponse
+    {
+        try {
+
+            $transactions = Transaction::where([
+                'card_id' => $id
+            ]);
+    
+            $response = [];
+            foreach($transactions  as $transaction){
+                array_push($response, $transaction);
+            }
+    
+            return response()->json($response, 200);
+
+        } catch(\Exception $e) {
+
+            $errorMessage = "Este cartão não possui transações.";
+            $response = [
+                "data" => [
+                    "error" => $errorMessage
+                ]
+            ];
+            return response()->json($response, 404);
+        }
+    }
+
+    public function showTransactionInstallments($id): JsonResponse
+    {
+        try {
+
+            $installments = Installment::where([
+                'transaction_id' => $id
+            ]);
+    
+            $response = [];
+            foreach($installments  as $installment){
+                array_push($response, $installment);
+            }
+    
+            return response()->json($response, 200);
+
+        } catch(\Exception $e) {
+
+            $errorMessage = "Esta transação não possui parcelas.";
+            $response = [
+                "data" => [
+                    "error" => $errorMessage
+                ]
+            ];
+            return response()->json($response, 404);
+        }
+
     }
 }
