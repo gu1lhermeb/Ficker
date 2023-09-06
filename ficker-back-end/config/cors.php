6bb1df08cdfffea01acf73a9e764eaf4e4eb0b76
Gbzzz <guilhermebraz089@gmail.com>
2023-09-06 19:25:14 +0000

    remove: cartão e transação no cartão da seed

--- ficker-back-end/config/cors.php
+++ ficker-back-end/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000', 'https://ficker-front.vercel.app/')],
 
     'allowed_origins_patterns' => [],
 
