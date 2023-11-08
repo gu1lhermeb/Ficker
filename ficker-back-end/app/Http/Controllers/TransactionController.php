5cea464819111f7d7513450fbe07bc3289b554ad
PHPdro <pedro110302@gmail.com>
2023-11-08 10:35:59 -0300

    fix: spendings middleware removed

--- ficker-back-end/app/Http/Controllers/TransactionController.php
+++ ficker-back-end/app/Http/Controllers/TransactionController.php
@@ -146,16 +146,27 @@ class TransactionController extends Controller
     {
         try {
             $transactions = Transaction::orderBy('date', 'desc')
-                ->where('user_id', Auth::user()->id)
+                ->where('user_id', Auth::id())
                 ->get();
 
-            $reponse = [
-                'data' => [
-                    'transactions' => $transactions
-                ]
-            ];
+            $most_expensive_transaction = Transaction::orderBy('transaction_value', 'desc')
+                ->where(['user_id'=> Auth::id(),
+                    'type_id' => 2])
+                ->first()->transaction_value;
 
-            return response()->json($reponse, 200);
+            $response = ['data' => ['transactions' => []]];
+
+            array_push($response['data']['transactions'], ['total' => count($transactions)]);
+            array_push($response['data']['transactions'], ['most_expensive' => $most_expensive_transaction]);
+
+            foreach($transactions as $transaction) {
+                $description = Category::find($transaction->category_id)->category_description;
+                $transaction->category_description = $description;
+                array_push($response['data']['transactions'], $transaction);
+            }
+
+            return response()->json($response, 200);
+
         } catch (\Exception $e) {
             $errorMessage = 'Nenhuma transação foi encontrada';
             $response = [
@@ -200,13 +211,16 @@ class TransactionController extends Controller
     public function showTransactionsByType($id): JsonResponse
     {
         try {
-
+            
             $transactions = Transaction::where([
                 'user_id' => Auth::user()->id,
                 'type_id' => $id
             ])->orderBy('date', 'desc')->get();
 
             $response = ['data' => ['transactions' => []]];
+
+            array_push($response['data']['transactions'], ['total' => count($transactions)]);
+
             foreach ($transactions  as $transaction) {
                 $description = Category::find($transaction->category_id)->category_description;
                 $transaction->category_description = $description;
